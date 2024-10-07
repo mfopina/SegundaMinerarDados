58ab96be4fb7748abffe71cbfe5ed25cef619608
Mário Pina <86622495+mfopina@users.noreply.github.com>
2024-10-07 16:31:17 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,12 +5,18 @@ async function vizualizarInformacaoGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas)*100)
 
  const paragrafo = document.createElement('p')   
  paragrafo.classList.add('graficos-container__texto')   
  
  paragrafo.innerHTML = `Você sabia que o mundo tem 
- ${dados.total_pessoas_conectadas}`
+ <span>${pessoasNoMundo} </span> de pessoas e aproximadamente
+ <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma
+ rede social e passam em média 
+ <span>${horas} </span> : <span>${minutos} </span> horas conectados`
 
 const container = document.getElementById("graficos-container")
 container.appendChild(paragrafo)
