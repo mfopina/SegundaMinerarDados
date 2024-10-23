a713c6ee4a77680fadb5e6a638dc297d86268b12
Mário Pina <86622495+mfopina@users.noreply.github.com>
2024-10-23 19:48:04 -0300

    Add files via upload

--- graficos/temperatura.js
+++ graficos/temperatura.js
@@ -1,4 +1,8 @@
-const url2 = "https://api.open-meteo.com/v1/forecast?latitude=-23.8389203&longitude=-50.2334137&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+var latitude =-23.8389203;
+var longitude = -50.2334137;
+
+const url2 = "https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+
 
 async function Temperatura(){
     
@@ -6,22 +10,23 @@ async function Temperatura(){
     const dados2 = await res2.json()
     console.log(dados2);
 
-    //const data = (dados2.current.time)
+    const data = (dados2.current.time)
 
-    const data = new Date(dados2.current.time(2023, 09, 20, 10, 11, 08));
-    
     const temperatura = (dados2.current.temperature_2m)
     const celsios = (dados2.hourly_units.temperature_2m)
 
  const paragrafo2 = document.createElement('p')   
- paragrafo2.classList.add('graficos-container__texto')   
+ paragrafo2.classList.add('graficos-container__texto')
+
  
  paragrafo2.innerHTML = `
  Dia  
- <!-- <span> ${data} </span> -->
- <span> ${data.toLocaleString('pt-BR', { timezone: 'UTC' })} </span>
+ <span> ${data} </span>
  Temperatura  
- <span> ${temperatura} </span>  <span> ${celsios} </span>  
+ <span> ${temperatura} </span>  <span> ${celsios} </span> 
+
+ Localização 
+ <span> ${latitude} </span>  <span> ${longitude} </span>
  
  `
 
